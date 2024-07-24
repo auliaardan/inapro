f6a13d7e7c9d592df2d2f696e604968679a69508
Ardan <aulia.rahman71@ui.ac.id>
2024-07-24 10:12:33 +0700

    updated Security

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
